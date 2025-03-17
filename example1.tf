6e74a8a72cee657dd1f1a88512d4312dc81af861
joe <joe@cs.dixie.edu>
2025-03-17 09:24:07 -0600

    add some var decllarations

--- example1.tf
+++ example1.tf
@@ -16,3 +16,17 @@ resource "aws_instance" "example" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+  description = "Your AWS Access Key ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+  description = "Your AWS Secret access key"
+}
+variable "AWS_SESSION_TOKEN" {
+  type = string
+  description = "Your AWS Session token"
+}
